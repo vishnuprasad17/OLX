881397c0c3e37dfeaf1e8f8a78327694179d3ed6
vishnuprasad17 <vishnu77781annur@gmail.com>
2024-01-02 01:40:14 +0530

    Conditional Rendering Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
@@ -13,31 +13,32 @@ import FirebaseContext, { authContext } from "./store/firebaseContext";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const { setUser } = useContext(authContext);
-
+  const { user, setUser } = useContext(authContext);
   const { app } = useContext(FirebaseContext);
 
   useEffect(() => {
     const auth = getAuth(app);
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
+    onAuthStateChanged(auth, (loggedInUser) => {
+      if (loggedInUser) {
+        setUser(loggedInUser);
       }
     });
-    
-  });
+  }, [app, setUser]);
 
   return (
     <div>
       <Post>
         <Router>
           <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/create" element={<Create />} />
+            <Route path="/" element={<Home />} />
+            {!user && <Route path="/signup" element={<Signup />} />}
+            {!user && <Route path="/login" element={<Login />} />}
+            {user && <Route path="/create" element={<Create />} />}
             <Route path="/view" element={<View />} />
+            <Route path="/signup" element={<Navigate to="/" />} />
+            <Route path="/login" element={<Navigate to="/" />} />
+            <Route path="/create" element={<Navigate to="/" />} />
           </Routes>
         </Router>
       </Post>
